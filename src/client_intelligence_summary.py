746ad1713c7225df033b02a83f1b45597dbd3d8d
ghoulstudios1320-design <ghoulstudios1320@gmail.com>
2026-06-13 14:37:56 -0700

    Create client_intelligence_summary.py

---
+++ src/client_intelligence_summary.py
@@ -0,0 +1,14 @@
+from src.write_client_intelligence_summary import (
+    build_summary,
+    main,
+    write_client_intelligence_summary,
+)
+
+__all__ = [
+    "build_summary",
+    "main",
+    "write_client_intelligence_summary",
+]
+
+if __name__ == "__main__":
+    main()
